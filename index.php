afea860144aeb24e5664e56beebc8540ceb29455
Zhang <QZhang@direkt-gruppe.de>
2017-07-17 12:05:11 +0200

    add meta tag
    add the viewport meta tag to <head>, in order to ensure proper rendering and touch zooming

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html >
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sun-Cashmere</title>
 	<link rel="shortcut icon" type="image/x-icon" href="img/tab-icon2.jpg">
 	<link rel="stylesheet" href="css/bootstrap.css">
